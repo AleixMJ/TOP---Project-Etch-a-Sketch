631fc1d754036d548ff7f443877b899ef7784e2b
AleixMJ <aleixtechIT@gmail.com>
2026-02-07 18:14:04 +0100

    Project finished
    Changed the logic with a function to create the grid createGrid() and added
    the requested button to created custom grids up to 100 size. Also, removed
    the internal borders so it looks like a white black sheet.

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const rows = 16;
-const cols = 16;
+
+let gridSize = 16;
+
+
+
 const container = document.getElementById("container");
 
-for (let i = 0; i <= cols; i++ ) {
-    const col = document.createElement("div");
+const newGrid = document.getElementById("gridCreation");
 
-    col.style.width  = "5vw";             
-    col.style.height = "auto";            
-    col.style.border = "1px solid black"; 
-    col.style.display = "flex";
-    col.style.flexDirection = "column";
 
-    console.log(`added col number ${i}`);
-    for (let j = 0; j <= rows; j++) {
-        const row = document.createElement("div");
-        row.style.width  = "auto";             
-        row.style.height = "5vh";            
-        row.style.border = "1px solid black"; 
-        row.style.display = "flex";
-        row.style.flexDirection = "row";
-        row.addEventListener("mouseenter", () => {row.classList.add("painted")});
-        col.appendChild(row);
-        
-        console.log(`added row number ${j}`);
+newGrid.addEventListener("click", () => {
+    gridSize = prompt("Enter grid Size (1-100):", "16");
+    container.replaceChildren();
+    createGrid(gridSize);
+
+})
+
+createGrid(16);
+
+// Creates the Grid
+function createGrid(gridSize) {
+    
+
+    for (let i = 0; i <= gridSize; i++ ) {        
+        const col = document.createElement("div");
+        col.style.width  = `${90/gridSize}vw`;             
+        col.style.height = "auto";            
+        col.style.display = "flex";
+        col.style.flexDirection = "column";
+
+        console.log(`added col number ${i}`);
+        for (let j = 0; j <= gridSize; j++) {
+            const row = document.createElement("div");
+            row.style.width  = "auto";             
+            row.style.height = `${90/gridSize}vh`;            
+            row.style.display = "flex";
+            row.style.flexDirection = "row";
+            row.addEventListener("mouseenter", () => {row.classList.add("painted")});
+            col.appendChild(row);
+            
+            console.log(`added row number ${j}`);
+        }
+        container.appendChild(col);
+        console.log('added row');
     }
-    container.appendChild(col);
-    console.log('added row');
 }
